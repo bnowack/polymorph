f0d5a04cb7216f490c2af2e851a9376106071dbf
Benjamin Nowack <mail@bnowack.de>
2016-10-04 10:18:30 +0200

    Define gherkin steps (#cucumber)

--- dev/e2e/config/hooks.js
+++ dev/e2e/config/hooks.js
@@ -5,7 +5,6 @@ module.exports = function () {
 
     this.Before(function (scenario, callback) {
 
-
         callback();
 
     });
--- dev/e2e/config/step-definitions.js
+++ dev/e2e/config/step-definitions.js
@@ -3,4 +3,28 @@
  */
 module.exports = function () {
 
+    this.When('I click on the header logo', function (callback) {
+        var self = this;
+        var selector = this.browser.hasShadowDom
+            ? 'polymorph-app-header::shadow a'
+            : 'polymorph-app-header a';
+        this.browser
+            .waitFor(selector)
+            .then(function () {
+                var $logo = $(self.browser.document).find(selector);
+                $logo.trigger('click');
+                $logo[0].click();
+                callback();
+            });
+    });
+
+    this.Then('I should see the home page', function (callback) {
+        var self = this;
+        this.browser
+            .waitFor('polymorph-page[path="/"]')
+            .then(function () {
+                callback();
+            });
+    });
+
 };
