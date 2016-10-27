3f39bf7e8cdb3bc512eb662658103fa52fd9af00
Benjamin Nowack <mail@bnowack.de>
2016-10-27 18:40:17 +0200

    Activate route for schema updates (#schema)

---
+++ src/Polymorph/Schema/SchemaController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Polymorph\Schema;
+
+use Polymorph\Application\Application;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Polymorph Schema Controller
+ *
+ */
+class SchemaController
+{
+
+    /**
+     * Checks/Updates the schema an generates a list of applied versions
+     *
+     * @param Application $app
+     * @return Response
+     */
+    public function handleSchemaVersionsRequest(Application $app)
+    {
+        $appliedVersions = $app['schema']->checkSchema();
+        $params = [
+            'pageTitle' => 'Schema Versions',
+            'content' => '
+                <h2>Applied Schema Versions</h2>
+                <ul>
+                    <li>' . join('</li></li>', $appliedVersions) . '</li>
+                </ul>
+            '
+        ];
+        $template = $app->config('templates')->content;
+        return $app->render($template, $params);
+    }
+
+}
