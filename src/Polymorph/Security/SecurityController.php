69501937660f98058e810baa02e3eeb6315e2b5b
Benjamin Nowack <mail@bnowack.de>
2017-01-31 08:35:10 +0100

    Add account info route (#security #account #route)

--- src/Polymorph/Security/SecurityController.php
+++ src/Polymorph/Security/SecurityController.php
@@ -3,6 +3,7 @@
 namespace Polymorph\Security;
 
 use Polymorph\Application\Application;
+use Symfony\Component\Security\Core\User\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,5 +14,29 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class SecurityController
 {
+
+    /**
+     * Provides information about currently logged-in user
+     *
+     * @param Application $app Application instance
+     * @param \stdClass $routeOptions The route definition as specified in the configuration file
+     *
+     * @return Response A JSON Response
+     */
+    public function handleAccountInfoRequest(Application $app, $routeOptions)
+    {
+        /* @var User $user */
+        $user = $app['users']->getCurrentUser();
+        $response = [
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles(),
+            'loginHref' => $routeOptions->loginHref,
+            'loginLabel' => $routeOptions->loginLabel,
+            'logoutHref' => $routeOptions->logoutHref,
+            'logoutLabel' => $routeOptions->logoutLabel,
+            'logoutToken' => $app['security']->getToken('logout')->getValue()
+        ];
+        return new JsonResponse($response);
+    }
 
 }
